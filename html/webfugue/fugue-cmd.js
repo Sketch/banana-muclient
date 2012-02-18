a81ff2fee0cf689bf66b3d89f6c33b16b3c5e471
Ryan Dowell <ssstarduster@gmail.com>
2012-02-17 22:59:58 -0600

    Improved Webfugue peel help files.

--- html/webfugue/fugue-cmd.js
+++ html/webfugue/fugue-cmd.js
@@ -70,40 +70,69 @@ addCommand('bow', function(args) {
   $.stylesheetSwitch('black-on-white');
 });
 addCommand('help', function(args) {
-  var hlp = [
+    var hlp = {};
+    hlp['help_'] = [
     "-- WebFugue help --",
-    "As of right now, the only commands that are supported are:",
+    "Welcome to the WebFugue peel for Banana!",
     "",
-    "/help - this screen",
+    "/help        - This screen",
+    "/help worlds - Help on commands for creating and navigating worlds.",
+    "/help other  - Help for the other possibly useful commands.",
     "",
-    "/wob - White-on-black style",
-    "/bow - Black-on-white style",
+    "The source code for Banana is available at:",
+    "https://github.com/captdeaf/banana-muclient",
     "",
-    "/open &lt;worldname&gt;",
-    "/connect &lt;host&gt; &lt;port&gt;",
-    "/disconnect",
-    "/close",
     "",
-    "/fg &lt;worldname&gt; - Display the named world, when you have more than one.",
+    "Using WebFugue (Quick start):",
     "",
-    "/limit &lt;pattern&gt; shows only lines matching the glob &lt;pattern&gt;.",
-    "/unlimit removes the pattern filter.",
+    "/open &lt;worldname&gt;       - Open a tab with the name &lt;worldname&gt;.",
+    "/connect &lt;host&gt; &lt;port&gt;  - In a tab, connect to telnet server.",
+    "Then connect to your character as normal!",
     "",
-    "ctrl+p or ctrl+up recalls previous command",
-    "ctrl+n or ctrl+down recalls next command",
+    "If you lose your connection to Webfugue, Banana's connection to worlds",
+    "will not be lost. Use /disconnect to disconnect from a server.",
     "",
-    "/setpassword <password> - Set a new password for Banana",
-    "",
-    "To connect to Walker's test mush for 'support':",
+    "--Example--",
+    "To connect to Walker's test mush for \"support\":",
     "",
     "/open walker",
     "/connect localhost 4240",
     "connect guest",
-    "",
-    "--"
+    "----"
   ];
-
-  for (var i = 0; i < hlp.length; i++) {
-    appendToWorld(curWorld, hlp[i]);
+  hlp['help_worlds'] = [
+    "-- World commands --",
+    "/open &lt;worldname&gt;       - Open a tab with the name &lt;worldname&gt;.",
+    "/connect &lt;host&gt; &lt;port&gt;  - In a tab, connect to telnet server.",
+    "/disconnect             - Disconnect from the current world.",
+    "/close                  - Close the current tab (disconnects from the world).",
+    "",
+    "/fg &lt;worldname&gt; - Display the named world, when you have more than one.",
+    "",
+    "If you lose your connection to Webfugue, Banana's connection to worlds",
+    "will not be lost. Use /disconnect to disconnect from a server.",
+  ];
+  hlp['help_other'] = [
+    "-- Other commands --",
+    "/wob - Change colors to white-on-black style",
+    "/bow - Change colors to black-on-white style",
+    "",
+    "/limit &lt;pattern&gt; shows only lines matching the glob &lt;pattern&gt;.",
+    "/unlimit removes the pattern filter.",
+    "",
+    "/setpassword &lt;password&gt; - Set a new password for your Banana web login.",
+    "",
+    "ctrl+p or ctrl+up recalls previous command",
+    "ctrl+n or ctrl+down recalls next command",
+  ];
+  hlp['help__notfound'] = [
+    "Help file for \"".concat(args).concat("\" not found.")
+  ];
+  file = "help_".concat(args);
+  if (! hlp.hasOwnProperty(file)) {
+    file = "help__notfound";
+  }
+  for (var i = 0; i < hlp[file].length; i++) {
+    appendToWorld(curWorld, hlp[file][i]);
   }
 });
